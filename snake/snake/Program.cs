808c5a8f47469106b5ee5aabca73c41623a443e4
Pablos <pablos@list.ru>
2022-04-19 13:19:14 +0300

    Lists generating practice

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -8,5 +8,21 @@ snake.Point p1 = new snake.Point(1,2,'*');
 p1.Draw();
 //snake.Point.Move(p1, 10, 10);
 Move(p1, 2, 3);
-p1.Draw();
+
+
+List<int> ls = new List<int>();
+ls.Add(10);
+ls.Add(11);
+ls.Add(21);
+
+ls.RemoveAt(0);
+foreach(int i in ls)
+{
+Console.WriteLine(i);
+}
+
+List<snake.Point> lP = new List<snake.Point>();
+lP.Add(p1);
+lP[0].Draw();
+
 Console.ReadLine();
